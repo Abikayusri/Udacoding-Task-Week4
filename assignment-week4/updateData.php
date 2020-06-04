d84387565ba744d8908b1482e46f5ff189144480
Abika Chairul Yusri <35727186+Abikayusri@users.noreply.github.com>
2020-06-05 00:53:14 +0700

    Update Data

--- assignment-week4/updateData.php
+++ assignment-week4/updateData.php
@@ -31,39 +31,3 @@
  
         echo json_encode($result);
     }
-?>
-
-/*
-    include_once('koneksi.php');
-
-    if(!empty($_POST['id_mahasiswa']) && !empty($_POST['mahasiswa_nama']) && !empty($_POST['mahasiswa_nohp'])&& !empty($_POST['mahasiswa_alamat'])){
-        
-        $id = $_POST['id_mahasiswa'];
-        $nama = $_POST['mahasiswa_nama'];
-        $nohp = $_POST['mahasiswa_nohp'];
-        $alamat = $_POST['mahasiswa_alamat'];
-
-        $query = "UPDATE mahasiswa set mahasiswa_nama = '$nama', mahasiswa_nohp = '$nohp', mahasiswa_ alamat = '$alamat' WHERE id_mahasiswa = '$id'";
-
-        $update = mysqli_query($connect, $query);
-
-        if($update) {
-            set_response(true, "Success update data");
-        } else {
-            set_response(false, "Failed update data");
-        }
-    } else {
-        set_response(false, "Id, Nama, Nomor HP, dan alamat harus diisi");  
-    }
-
-
-    function set_response($isSuccess, $message)
-    {
-        $result = array(
-            'isSuccess' => $isSuccess,
-            'message' => $message
-        );
- 
-        echo json_encode($result);
-    } 
-*/
